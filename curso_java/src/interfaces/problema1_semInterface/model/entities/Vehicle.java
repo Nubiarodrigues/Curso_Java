120c8e5acc5c8a6f170dfe143023e205985db626
Nubia Rodrigues <nubiarodrigues2223@gmail.com>
2025-01-08 14:12:31 -0300

    curso_java

---
+++ curso_java/src/interfaces/problema1_semInterface/model/entities/Vehicle.java
@@ -0,0 +1,25 @@
+package interfaces.problema1_semInterface.model.entities;
+
+public class Vehicle {
+	
+	private String model;
+	
+	public Vehicle() {}
+
+	public Vehicle(String model) {
+		this.model = model;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	
+	
+	
+
+}
